5492262af2d1cd0fbdd9ca21ea6f83a0166ee2f3
carmensiay0316 <csia0019@student.monash.edu>
2026-05-20 14:28:45 +1000

    Update main.js

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ vegaEmbed("#bar_country_2025", "vega/bar_country_2025.json", { actions: false })
 vegaEmbed("#map_state_2025", "vega/map_state_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#sector_area_2019_2025", "vega/sector_area_2019to2025.json", { actions: false })
+vegaEmbed("#map_symbol_state_2025", "vega/map_symbol_state_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#sector_bump_2019_2025", "vega/sector_bump_2019to2025.json", { actions: false })
+vegaEmbed("#sector_area_2019_2025", "vega/sector_area_2019_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#map_symbol_state_2025", "vega/symbol_map_state_2025.json", { actions: false })
+vegaEmbed("#sector_bump_2019_2025", "vega/sector_bump_2019_2025.json", { actions: false })
   .catch(console.error);
