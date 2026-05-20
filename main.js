ed7a8853ecf7f6ec11f1e46cf0e1bead0568f2bf
carmensiay0316 <csia0019@student.monash.edu>
2026-05-20 14:31:16 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-vegaEmbed("#bar_state_2025", "vega/bar_state_2025.json", { actions: false })
+vegaEmbed("#bar_state_2025", "bar_state_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#bar_country_2025", "vega/bar_country_2025.json", { actions: false })
+vegaEmbed("#bar_country_2025", "bar_country_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#map_state_2025", "vega/map_state_2025.json", { actions: false })
+vegaEmbed("#map_state_2025", "map_state_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#map_symbol_state_2025", "vega/map_symbol_state_2025.json", { actions: false })
+vegaEmbed("#map_symbol_state_2025", "symbol_map_state_2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#sector_area_2019_2025", "vega/sector_area_2019_2025.json", { actions: false })
+vegaEmbed("#sector_area_2019_2025", "sector_area_2019to2025.json", { actions: false })
   .catch(console.error);
 
-vegaEmbed("#sector_bump_2019_2025", "vega/sector_bump_2019_2025.json", { actions: false })
+vegaEmbed("#sector_bump_2019_2025", "sector_bump_2019to2025.json", { actions: false })
   .catch(console.error);
